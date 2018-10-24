51813330e638748e1e7c5df4a1a4dd0bb045c8ab
Denys Gudakov <denysgud@gmail.com>
2018-10-25 01:37:59 +0300

    Created info block

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,95 @@
 <template>
-  <section class="page">
-    <div class="page-row">
+  <section class="page page_home">
+    <div class="page-block">
       <div class="page__container">
-        <div class="page__column">
-          <h1 class="page__title">
-            Contacts
-          </h1>
-        </div>
-        <div class="page__column page__column_half">
-          <div class="address">
-            Address
+        <div class="row row_with-padding">
+          <div class="row__column">
+            <h1 class="page__title">
+              Contacts
+            </h1>
           </div>
-        </div>
-        <div class="page__column page__column_half">
-          <div class="support">
-            support
+          <div class="row__column row__column_half">
+            <div class="address">
+              <h2 class="page-block__title">
+                <span class="text-part text-part_green">GMS</span> Headquarters
+              </h2>
+              <div class="row">
+                <div class="row__column row__column_half">
+                  <p class="text">
+                    Global Message Services Ukraine LLC<br>
+                    Bundesstrasse 5 | 3rd Floor<br>
+                    6300 Zug<br>
+                    Switzerland<br>
+                  </p>
+                  <a
+                    class="link link_tel"
+                    href="tel:+41415446200">+41 41 544 62 00</a>
+                </div>
+                <div class="row__column row__column_half">
+                  <p class="text">
+                    Email<br>
+                    <a 
+                      class="link"
+                      href="mailto:info@gms-worldwide.com">info@gms-worldwide.com</a><br>
+                    Send message<br>
+                    <a
+                      class="link"
+                      href="viber://add?number=41415446200">viber us</a><br>
+                    Send form<br>
+                    <a
+                      class="link"
+                      href="#form">Contact us</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row__column row__column_half">
+            <div class="support">
+              <h2 class="page-block__title">
+                Technical support <span class="text-part text-part_green">24/7</span>
+              </h2>
+              <p class="text">
+                GMS understands that it can be hard to follow all new technologies.
+                We regularly provide for our clients educational trainings to male your
+                operator's business earn more and better though our best practices.
+              </p>
+              <a
+                class="link link_tel"
+                href="tel:+41415446206">+41 41 544 62 06</a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="page-row page-row_grey">
+    <div class="page-block page-block_offices page-block_no-padding">
       <div class="page__container">
-        <div class="page__column page__column_half">
-          <div class="offices">
-            offices
-          </div>
-        </div> 
-        <div class="page__column page__column_half">
-          <div class="map">
-            map
+        <div class="row row_with-padding">
+          <div class="row__column row__column_half">
+            <div class="offices">
+              offices
+            </div>
+          </div> 
+          <div class="row__column row__column_half">
+            <div class="map">
+              map
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="page-row page-row_pattern">
+    <div class="page-block page-block_form">
       <div class="page__container">
-        <div class="page__column page__column_half">
-          <div class="form">
-            form
+        <div class="row">
+          <div class="row__column row__column_half">
+            <div class="form">
+              form
+            </div>
           </div>
-        </div>
-        <div class="page__column page__column_half">
-          <div class="request-info">
-            text
+          <div class="row__column row__column_half">
+            <div class="request-info">
+              text
+            </div>
           </div>
         </div>
       </div>
